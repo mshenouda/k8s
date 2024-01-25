99b802cfe03a52cf5e55f2576120f302f04472ed
Mina Shenouda <mina.hanna.morris@gmail.com>
2024-01-25 15:04:16 -0800

    first commit

---
+++ k8s-web-to-nginx/index.mjs
@@ -0,0 +1,22 @@
+import express from 'express';
+import os from 'os';
+import fetch from 'node-fetch';
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res)=>{
+    const helloMessage = `Hello from ${os.hostname()}`;
+    res.send(helloMessage);
+});
+
+app.get('/nginx', async (req, res)=>{
+    const url = "http://nginx";
+    const response = await fetch(url);
+    const body = await response.text();
+    res.send(body);
+});
+
+app.listen(PORT, ()=>{
+    console.log(`Web server is listening on PORT ${PORT}`);
+});
